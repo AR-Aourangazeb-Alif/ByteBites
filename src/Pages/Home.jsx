772865452b6e60bf10fddee4b42a4297660b662c
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-03-24 01:19:32 +0600

    ...

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import Nav from "../Components/Nav";
+import Blogs from "./Blogs";
+import Bookmarks from "./Bookmarks";
 
 const Home = () => {
     return (
         <div>
             <Nav></Nav>
+
+            <Blogs></Blogs>
+
+            <Bookmarks></Bookmarks>
 
         </div>
     );
